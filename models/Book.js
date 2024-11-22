0b159d0e8b79cc58d1561d4a39874ce23e35522e
Sophie Martin <pub.et.co18@gmail.com>
2024-11-22 10:45:12 +0100

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Schéma flexible pour un livre
-const bookSchema = new Schema({
-  title: { type: String },
+
+// Schéma pour les manuscrits
+const bookSchema = new mongoose.Schema({
+  url: { type: String, required: true },// URL comme champ
+  Title: { type: String },
   author: { type: String },
   isbn: { type: String },
   genre: { type: String },
   publishedYear: { type: Number },
   availableCopies: { type: Number, default: 0 },
-  pdfBook: { type: mongoose.Schema.Types.ObjectId, ref: 'fs.files' } // Référence à GridFS
+  pdfBook: { type: mongoose.Schema.Types.ObjectId, ref: 'fs.files' }, 
+  DigitisedBy: String,
+  Shelfmark: String,
+  Date: mongoose.Schema.Types.Mixed, // Accepte soit une chaîne, soit un tableau d'objets
+  Language: [String],
+  Format: String,
+  Creator: String,
+  Type: String,
+  Place: mongoose.Schema.Types.Mixed, // Peut être un objet ou une chaîne
 }, {
   timestamps: true,
   strict: false // Permet l'ajout de champs non définis dans le schéma
